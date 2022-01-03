51e3ad7f4ab9990fd63a89c9d859d86c90b64dc4
Chung Tran <bigearsenal@gmail.com>
2022-01-03 16:38:34 +0700

    refactor: changeRootViewController method

--- p2p_wallet/Scenes/AppCoordinator.swift
+++ p2p_wallet/Scenes/AppCoordinator.swift
@@ -11,10 +11,12 @@ import UIKit
 class AppCoordinator {
     // MARK: - Properties
     private let window: UIWindow?
+    private let storage: AccountStorageType & PincodeStorageType & NameStorageType
     
     // MARK: - Initializer
     init(window: UIWindow?) {
         self.window = window
+        self.storage = Resolver.resolve()
     }
     
     // MARK: - Methods
@@ -24,8 +26,31 @@ class AppCoordinator {
     
     func reload() {
         // set placeholder vc
-        window?.rootViewController = PlaceholderViewController()
+        changeRootViewController(to: PlaceholderViewController())
         
-        
+        // try to retrieve account from seed
+        DispatchQueue.global(qos: .userInteractive).async { [weak self] in
+            let account = self?.storage.account
+            DispatchQueue.main.async { [weak self] in
+                if account == nil {
+//                    self?.showAuthenticationOnMainOnAppear = false
+//                    self?.navigationSubject.accept(.createOrRestoreWallet)
+                } else if self?.storage.pinCode == nil ||
+                            !Defaults.didSetEnableBiometry ||
+                            !Defaults.didSetEnableNotifications
+                {
+//                    self?.showAuthenticationOnMainOnAppear = false
+//                    self?.navigationSubject.accept(.onboarding)
+                } else {
+//                    self?.navigationSubject.accept(.main(showAuthenticationWhenAppears: self?.showAuthenticationOnMainOnAppear ?? false))
+                }
+            }
+        }
+    }
+    
+    // MARK: - Helpers
+    private func changeRootViewController(to vc: UIViewController) {
+        // TODO: - Animation
+        window?.rootViewController = vc
     }
 }
